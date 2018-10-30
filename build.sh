1fcb515f0cde058fa94b56315cdbdbd78e24df3a
Lisa LIU <mayerlbp@hotmail.com>
2018-10-30 13:23:56 -0400

    Add docker pull

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ echo "---------------build ./nb-redis---------------"
 cd ./nb-redis
 docker image build -t cg2-linux-asg/redis -f Dockerfile .
 docker tag cg2-linux-asg/redis:latest 041444721655.dkr.ecr.us-east-1.amazonaws.com/cg2-linux-asg/redis:latest
+docker push 041444721655.dkr.ecr.us-east-1.amazonaws.com/cg2-linux-asg/redis:latest
 
 
 cp readme ../output/nb-redis/
@@ -22,6 +23,7 @@ echo "---------------build ./nb-taskengine---------------"
 cd ../nb-taskengine
 docker image build -t cg2-linux-asg/task-engine -f Dockerfile .
 docker tag cg2-linux-asg/task-engine:latest 041444721655.dkr.ecr.us-east-1.amazonaws.com/cg2-linux-asg/task-engine:latest
+docker push 041444721655.dkr.ecr.us-east-1.amazonaws.com/cg2-linux-asg/task-engine:latest
 
 cp readme ../output/nb-taskengine/
 cp -r conf ../output/nb-taskengine/
